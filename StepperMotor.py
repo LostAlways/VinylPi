1ee9a2bb15bfd3ea54ea119cd91ae85fb143e94b
DavidM <115198671+LostAlways@users.noreply.github.com>
2024-02-11 19:26:40 +1100

    Update StepperMotor.py

--- StepperMotor.py
+++ StepperMotor.py
@@ -1,83 +1,105 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 import sys
 import os
-
-# Specify the path to your virtual environment
-venv_path = '/home/VinylPi/Desktop/rfidspotify/env'
-
-# Add the virtual environment's site-packages directory to sys.path
-sys.path.append(os.path.join(venv_path, 'lib', 'python3.11', 'site-packages'))
-
+from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
+from time import sleep
 import time
 
+# ... (previous code)
 
+def setup_stepper_motor():
+    in1 = 4
+    in2 = 17
+    in3 = 27
+    in4 = 22
 
-in1 = 4
-in2 = 17
-in3 = 27
-in4 = 22
+    step_sleep = 0.002
+    step_count = 4096
+    direction = False
+    step_sequence = [[1, 0, 0, 1],
+                     [1, 0, 0, 0],
+                     [1, 1, 0, 0],
+                     [0, 1, 0, 0],
+                     [0, 1, 1, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 1, 1],
+                     [0, 0, 0, 1]]
 
-# careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
-step_sleep = 0.002
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(in1, GPIO.OUT)
+    GPIO.setup(in2, GPIO.OUT)
+    GPIO.setup(in3, GPIO.OUT)
+    GPIO.setup(in4, GPIO.OUT)
 
-step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
 
-direction = False # True for clockwise, False for counter-clockwise
+    motor_pins = [in1, in2, in3, in4]
+    motor_step_counter = 0
 
-# defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
-step_sequence = [[1,0,0,1],
-                 [1,0,0,0],
-                 [1,1,0,0],
-                 [0,1,0,0],
-                 [0,1,1,0],
-                 [0,0,1,0],
-                 [0,0,1,1],
-                 [0,0,0,1]]
+    return (motor_pins, step_sequence, motor_step_counter, step_sleep, direction, step_count)
 
-# setting up
-GPIO.setmode( GPIO.BCM )
-GPIO.setup( in1, GPIO.OUT )
-GPIO.setup( in2, GPIO.OUT )
-GPIO.setup( in3, GPIO.OUT )
-GPIO.setup( in4, GPIO.OUT )
+def rotate_stepper_motor(motor_pins, step_sequence, motor_step_counter, step_sleep, direction, step_count):
+    try:
+        i = 0
+        for i in range(step_count):
+            for pin in range(0, len(motor_pins)):
+                GPIO.output(motor_pins[pin], step_sequence[motor_step_counter][pin])
+            if direction == True:
+                motor_step_counter = (motor_step_counter - 1) % 8
+            elif direction == False:
+                motor_step_counter = (motor_step_counter + 1) % 8
+            else:  # defensive programming
+                print("uh oh... direction should *always* be either True or False")
+                cleanup()
+                exit(1)
+            time.sleep(step_sleep)
 
-# initializing
-GPIO.output( in1, GPIO.LOW )
-GPIO.output( in2, GPIO.LOW )
-GPIO.output( in3, GPIO.LOW )
-GPIO.output( in4, GPIO.LOW )
+    except KeyboardInterrupt:
+        cleanup()
+        exit(1)
 
-motor_pins = [in1,in2,in3,in4]
-motor_step_counter = 0 ;
+def main():
+    servo = setup_gpio_and_servo()
+    motor_params = setup_stepper_motor()
 
-def cleanup():
-    GPIO.output( in1, GPIO.LOW )
-    GPIO.output( in2, GPIO.LOW )
-    GPIO.output( in3, GPIO.LOW )
-    GPIO.output( in4, GPIO.LOW )
-    GPIO.cleanup()
+    try:
+        while True:
+            reader = SimpleMFRC522()
 
-# the meat
-try:
-    i = 0
-    for i in range(step_count):
-        for pin in range(0, len(motor_pins)):
-            GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
-        if direction==True:
-            motor_step_counter = (motor_step_counter - 1) % 8
-        elif direction==False:
-            motor_step_counter = (motor_step_counter + 1) % 8
-        else: # defensive programming
-            print( "uh oh... direction should *always* be either True or False" )
-            cleanup()
-            exit( 1 )
-        time.sleep( step_sleep )
+            try:
+                print("Waiting for RFID card...")
+                id, text = reader.read()
 
-except KeyboardInterrupt:
-    cleanup()
-    exit( 1 )
+                print("Card detected! Moving servo to 70 degrees.")
+                move_servo(servo, 70)
 
-cleanup()
-exit( 0 )
+                print("Rotating stepper motor continuously.")
+                rotate_stepper_motor(*motor_params)
+
+                # Do something with the RFID card data (e.g., Spotify integration)
+
+            except Exception as e:
+                print(f"Error reading RFID card: {e}")
+                print("Moving servo back to 0 degrees.")
+                move_servo(servo, 0)
+                # Stop the stepper motor rotation
+                cleanup()
+
+            finally:
+                sleep(2)
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        servo.stop()
+        GPIO.cleanup()
+        print("Goodbye")
+
+if __name__ == "__main__":
+    main()
